277a2df3b346f34d3512cc61400d84c4ff885e74
Sayanjit Jana <sayanjitjana373@gmail.com>
2026-07-28 21:08:32 +0530

    First Hello World Program

---
+++ Hello_World.cpp
@@ -0,0 +1,24 @@
+// ? With using namespace std;:
+// * std is the standard namespace in C++.
+// * It allows you to use cout, cin, endl, etc., without writing std:: every time.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello";
+    return 0;
+}
+
+// ? Without using namespace std;:
+
+
+/*
+#include <iostream>
+
+int main() {
+    std::cout << "Hello";
+    return 0;
+}
+
+*/
