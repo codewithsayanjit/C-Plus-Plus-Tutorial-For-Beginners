40c9294a23c9bf7c28830eead0c5cf52e261d4f7
Sayanjit Jana <sayanjitjana373@gmail.com>
2026-08-01 20:32:00 +0530

    Basic Concepts in Object-Oriented Programming

---
+++ Object_Oriented_Programming.cpp
@@ -0,0 +1,32 @@
+/*
+* Difference between Procedure Oriented Programming and Object-Oriented Programming
+
+    ? Procedure Oriented Programming :
+        Consists of writing a set of instruction for the computer to follow
+        The main focus is on functions and not on the flow of data
+        Functions can either use local or global data
+        Data moves openly from function to function
+
+    ? Object-Oriented Programming :
+        Works on the concept of classes and object
+        A class is a template to create objects
+        Treats data as a critical element
+        Decomposes the problem in objects and builds data and functions around the objects
+
+
+* Basic Concepts in Object-Oriented Programming
+    ? Classes - Basic template for creating objects
+    ? Objects – Basic run-time entities
+    ? Data Abstraction & Encapsulation – Wrapping data and functions into a single unit
+    ? Inheritance – Properties of one class can be inherited into others
+    ? Polymorphism – Ability to take more than one forms
+    ? Dynamic Binding – Code which will execute is not known until the program runs
+    ? Message Passing – message (Information) call format
+
+
+* Benefits of Object-Oriented Programming
+    ? Better code reusability using objects and inheritance
+    ? Principle of data hiding helps build secure systems
+    ? Multiple Objects can co-exist without any interference
+    ? Software complexity can be easily managed
+*/
